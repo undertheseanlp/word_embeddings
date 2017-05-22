742ed37888cca3ba8164bed93a3adf165af534a1
Vu Anh <brother.rain.1024@gmail.com>
2017-05-22 17:02:53 +0700

    fix data sync

--- task_data_sync.py
+++ task_data_sync.py
@@ -1,3 +1,16 @@
+from os import mkdir
 from dirsync import sync
-sync("corpus/corpus.vinews/vn_news/data", "corpus/corpus.vinews.data", "sync")
-sync("corpus/corpus.viwiki/viwiki", "corpus/corpus.viwiki.data", "sync")
+
+try:
+    mkdir("corpus/corpus.vinews.data")
+except:
+    pass
+finally:
+    sync("corpus/corpus.vinews/vn_news/data", "corpus/corpus.vinews.data", "sync")
+
+try:
+    mkdir("corpus/corpus.viwiki.data")
+except:
+    pass
+finally:
+    sync("corpus/corpus.viwiki/viwiki", "corpus/corpus.viwiki.data", "sync")
