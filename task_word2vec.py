5be62726ad815522f71c42b5d277e635a10bc2c3
Vu Anh <brother.rain.1024@gmail.com>
2017-05-22 15:20:35 +0700

    update doc

--- task_word2vec.py
+++ task_word2vec.py
@@ -28,8 +28,8 @@ def log(s):
 
 
 if __name__ == '__main__':
-    documents = load_documents("corpus.pending")
-    documents.extend(load_documents("corpus\\corpus.viwiki.segmented"))
+    documents = load_documents(join("corpus", "corpus.vinews.segmented"))
+    documents.extend(load_documents(join("corpus", "corpus.viwiki.segmented")))
 
     sentences = [get_sentence(document) for document in documents]
     sentences = [s for sublist in sentences for s in sublist]
@@ -39,14 +39,14 @@ if __name__ == '__main__':
     model = gensim.models.Word2Vec(sentences, size=params["size"], min_count=params["min_count"])
     vocab = list(model.wv.vocab.keys())
 
-    log_file = "logs\\%s.txt" % datetime.now().strftime('%Y-%m-%d_%H-%M')
+    log_file = join("logs", "%s.txt" % datetime.now().strftime('%Y-%m-%d_%H-%M'))
     f = io.open(log_file, "w", encoding="utf-8")
 
     log(u"Params: " + unicode(params))
     log(u"Vocab Size: %d\n" % len(vocab))
 
     words = [u"Hà Nội", u"đặc biệt", u"quyết tâm", u"khó khăn", u"cha", u"gái", u"Microsoft", u"Apple", u"Samsung"]
-    model_file = "model\\word2vec-%s.model" % datetime.now().strftime('%Y-%m-%d_%H')
+    model_file = join("model", "word2vec-%s.model" % datetime.now().strftime('%Y-%m-%d_%H'))
     model.save(model_file)
 
     for word in words:
